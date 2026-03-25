69a104c2470465a4c60ba2461dd2b29027dcc316
hnsu76579-coder <hnsu76579@gmail.com>
2026-03-26 00:32:25 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
@@ -16,5 +17,6 @@ createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <App />
     <Analytics/>
+    <SpeedInsights/>
   </QueryClientProvider>
 )
